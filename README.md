38fb9d5b67d91ad6c3da9dd5ce93370d2fc9b72b
Florian Fida <f+neno@fida.biz>
2015-07-13 21:22:41 +0200

    TryFinally

---
+++ README.md
@@ -0,0 +1,1 @@
+![Screenshot](https://raw.githubusercontent.com/piccaso/TryFinally/master/screenshot.png)
---
+++ TryFinally/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryFinally {
+
+    class Program {
+        static void Main(string[] args) {
+            int i = int.Parse(args[0]);
+            try {
+                iWillThrowUp(ref i);
+            } catch(Exception e) {
+                Console.WriteLine("EX:" + e.Message);
+            }
+            Console.WriteLine("i:" + i.ToString());
+        }
+        static void iWillThrowUp(ref int i) {
+            try {
+                if(i>0 || i<0) throw new Exception("OMG!");
+            } finally {
+                i++;
+            }
+        }
+    }
+}
